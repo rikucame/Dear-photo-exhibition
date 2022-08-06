7a4723d0eec5620d2c2c1dd6afef2eb87a59ca24
Henry <rikucame@gmail.com>
2022-08-07 00:59:53 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,63 +1,3 @@
-const today = new Date().getDate();
-
-window.onload = () => {
-  const leftArrow = document.getElementById("left-arrow");
-  const rightArrow = document.getElementById("right-arrow");
-  rightArrow.classList.add("onDisplay");
-  document.getElementById("film").onscroll = function () {
-    scrollValue = this.scrollLeft;
-    if (scrollValue < 10) {
-      leftArrow.classList.remove("onDisplay");
-      rightArrow.classList.add("onDisplay");
-    } else if (scrollValue >= 10 && scrollValue < 1600) {
-      leftArrow.classList.add("onDisplay");
-      rightArrow.classList.add("onDisplay");
-    } else if (scrollValue >= 1600) {
-      leftArrow.classList.add("onDisplay");
-      rightArrow.classList.remove("onDisplay");
-    }
-  };
-  displayTodaysExhibitors();
-};
-
-const displayTodaysExhibitors = () => {
-  const mainVisualsWrap = document.getElementById("mainVisualsWrap");
-
-  const maxNum = today - 5;
-  members.slice(0, maxNum).map((member) => {
-    mainVisualsWrap.prepend(generateMainVisual(member));
-  });
-};
-
-const generateMainVisual = (data) => {
-  // 要素生成
-  const wrap = document.createElement("div");
-  const link = document.createElement("a");
-  const image = document.createElement("img");
-  const shadow = document.createElement("span");
-  const name = document.createElement("p");
-
-  // クラス設定
-  wrap.className = "frame-wrap";
-  link.className = "frame";
-  image.className = `main-visual-image-${data.direction}`;
-  shadow.className = "shadow";
-  name.className = "name";
-
-  // attributeの設定
-  link.href = `./members/${data.name}.html`;
-  image.src = `./images/main_visuals/${data.mainVisual}`;
-  name.innerHTML = data.displayName;
-
-  // 結合
-  link.appendChild(image);
-  link.appendChild(shadow);
-  wrap.appendChild(link);
-  wrap.appendChild(name);
-
-  return wrap;
-};
-
 const members = [
   {
     name: "hana",
@@ -120,3 +60,64 @@ const members = [
     direction: "vertical",
   },
 ];
+
+const today = new Date().getDate();
+const mainVisuals = members.slice(0, today - 5).map((data) => {
+  // 要素生成
+  const wrap = document.createElement("div");
+  const link = document.createElement("a");
+  const image = document.createElement("img");
+  const shadow = document.createElement("span");
+  const name = document.createElement("p");
+
+  // クラス設定
+  wrap.className = "frame-wrap";
+  link.className = "frame";
+  image.className = `main-visual-image-${data.direction}`;
+  shadow.className = "shadow";
+  name.className = "name";
+
+  // attributeの設定
+  link.href = `./members/${data.name}.html`;
+  image.src = `./images/main_visuals/${data.mainVisual}`;
+  name.innerHTML = data.displayName;
+
+  // 結合
+  link.appendChild(image);
+  link.appendChild(shadow);
+  wrap.appendChild(link);
+  wrap.appendChild(name);
+
+  return wrap;
+});
+
+window.onload = () => {
+  togleDisplayArrows();
+  displayTodaysExhibitors();
+};
+
+const togleDisplayArrows = () => {
+  const left = document.getElementById("left-arrow").classList;
+  const right = document.getElementById("right-arrow").classList;
+  // 初期化
+  right.add("onDisplay");
+
+  document.getElementById("film").onscroll = function () {
+    scrollValue = this.scrollLeft;
+    if (scrollValue < 10) {
+      left.remove("onDisplay");
+      right.add("onDisplay");
+    } else if (scrollValue >= 10 && scrollValue < 1600) {
+      left.add("onDisplay");
+      right.add("onDisplay");
+    } else if (scrollValue >= 1600) {
+      left.add("onDisplay");
+      right.remove("onDisplay");
+    }
+  };
+};
+
+const displayTodaysExhibitors = () => {
+  const mainVisualsWrap = document.getElementById("mainVisualsWrap");
+  mainVisuals.forEach((elm) => mainVisualsWrap.prepend(elm));
+};
